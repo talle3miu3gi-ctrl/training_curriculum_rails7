dce9f21427a40a4fef43263e39cbe4b051b099a4
talle3miu3gi-ctrl <talle3miu3gi@gmail.com>
2026-04-09 21:38:09 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,10 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = (@todays_date + x).wday
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}#wdaysから値を取り出す記述
       @week_days.push(days)
     end
 
